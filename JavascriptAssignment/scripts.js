eb0971da7385e84112e37a8d4bd785638ac66337
unknown <vr14pa@LABS-WP017.campus.brocku.local>
2015-12-03 17:10:15 -0500

    New One

--- JavascriptAssignment/scripts.js
+++ JavascriptAssignment/scripts.js
@@ -59,6 +59,11 @@ function getValue(resolve){
 	else if(resolve == "three"){resolve = 3;}
 	else if(resolve == "four"){resolve = 4;}
 	else if(resolve == "five"){resolve = 5;}
+  else if(resolve == "six"){resolve = 6;}
+  else if(resolve == "seven"){resolve = 7;}
+  else if(resolve == "eight"){resolve = 8;}
+  else if(resolve == "nine"){resolve = 9;}
+  else if(resolve == "ten"){resolve = 10;}
 	else{resolve = "Not a Number";}
 
 	return resolve;
